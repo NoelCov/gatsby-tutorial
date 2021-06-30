afa036f396922497b8bf67d329db5f8d0e6012e9
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-06-30 16:45:46 -0700

    Finished Gatsby tutorial part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,15 @@ import * as React from "react";
 
 import Layout from "../components/layout";
 
+import { StaticImage } from "gatsby-plugin-image";
+
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Page">
-     <p>This is the about page</p> 
+      <p>This is the about page</p>
+      <StaticImage src="../images/money.png" alt="Money stack" />
     </Layout>
-  )
-}
+  );
+};
 
 export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@ import * as React from "react";
 
 import Layout from "../components/layout";
 
+import { StaticImage } from "gatsby-plugin-image";
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-    <p>I'm learning Gatsby!!!!</p>
+      <p>I'm learning Gatsby!!!!</p>
+      <StaticImage
+        alt="GatsbyJS Logo"
+        src="https://www.whitespectre.com/uploads/gatsby.jpg"
+      />
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
